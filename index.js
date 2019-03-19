1124f2046de792de4d74e34494f0bf42255c0968
andrew.walker123 <andrew.walker3@sky.uk>
2019-03-19 15:41:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+let distanceFromHqInBlocks = blockNumber => {
+    return Math.abs(blockNumber - 42)
+};
+
+let distanceFromHqInFeet = blockNumber => {
+    let blocksAway = distanceFromHqInBlocks(blockNumber)
+    return blocksAway*264
+};
+
+let distanceTravelledInFeet = (startBlock, endBlock) => {
+    return Math.abs((endBlock - startBlock) * 264)
+};
+
+let calculatesFarePrice = (startBlock, endBlock) => {
+
+    let distanceTravelled = distanceTravelledInFeet(startBlock, endBlock)
+
+    if (distanceTravelled>2500) {return "cannot travel that far"}
+    if (distanceTravelled<400) {return 0}
+    if (distanceTravelled>2000) {return 25}
+    if (distanceTravelled>400 && distanceTravelled<2000) {return (distanceTravelled - 400) * 0.02}
+};
