9131eb727c2338bd26b04df43971a010a0a08082
Christopher Orrick <christopherorrick@me.com>
2019-03-19 15:43:01 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -58,6 +58,7 @@ describe('index.js', function() {
     });
 
     it('does not allow rides over 2500 feet', function() {
+      console.log(calculatesFarePrice(34, 24));
       expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
     });
   });
