3e7b7ef6d89c1c99b55a85a94787b0134def2f3d
Anna Burley <anna.burley@sky.uk>
2019-03-19 16:10:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const hQ = 42;
+const oneBlockDistance = 264;
+const distanceFromHqInBlocks = (block) => {
+  let blocks;
+  if(block > 42){
+    blocks = block - hQ;
+  } else {
+    blocks = hQ - block;
+  }
+  return blocks;
+};
+
+
+const distanceFromHqInFeet = (block) => {
+  return distanceFromHqInBlocks(block) * oneBlockDistance;
+};
+
+const distanceTravelledInFeet = (startingBlock, endingBlock) => {
+  let numberOfBlocks = Math.abs(endingBlock - startingBlock);
+  return numberOfBlocks * oneBlockDistance;
+};
+
+const calculatesFarePrice = (startingBlock, endingBlock) => {
+  const freeDistance = 400;
+  const distance = distanceTravelledInFeet(startingBlock, endingBlock);
+  if(distance < 400) {
+    return 0;
+  } else if(distance > 400 && distance < 2000) {
+    return (distance - freeDistance) * 0.02;
+  } else if(distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
